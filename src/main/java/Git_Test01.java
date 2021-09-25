9a284635875efdac129b506327e157e63a8e964d
sht <sht@163.com>
2021-09-25 16:10:41 +0800

    first commit

---
+++ src/main/java/Git_Test01.java
@@ -0,0 +1,11 @@
+/**
+ * @Author :
+ * @description :
+ * @DateTime : 2021-09-25 16:05
+ * @Version 1.0
+ */
+public class Git_Test01 {
+    public static void main(String[] args) {
+        System.out.println("this is git test");
+    }
+}
